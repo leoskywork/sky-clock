520b42dc4d36efe4f883d1e41c29167d0b35c6c6
Leo <leoskywork@outlook.com>
2021-10-02 23:01:05 +0800

    initial

---
+++ BigClock/Main.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BigClock
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelWeek = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTime.Font = new System.Drawing.Font("SimSun", 120F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTime.Location = new System.Drawing.Point(0, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(902, 228);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "time";
+            this.labelTime.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelTime_MouseDown);
+            // 
+            // timerMain
+            // 
+            this.timerMain.Interval = 30000;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // labelDate
+            // 
+            this.labelDate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelDate.Font = new System.Drawing.Font("SimSun", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelDate.Location = new System.Drawing.Point(0, 433);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.labelDate.Size = new System.Drawing.Size(902, 200);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "date";
+            // 
+            // labelWeek
+            // 
+            this.labelWeek.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelWeek.Font = new System.Drawing.Font("SimSun", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelWeek.Location = new System.Drawing.Point(0, 228);
+            this.labelWeek.Name = "labelWeek";
+            this.labelWeek.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.labelWeek.Size = new System.Drawing.Size(902, 205);
+            this.labelWeek.TabIndex = 2;
+            this.labelWeek.Text = "week";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(839, 12);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(50, 50);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "Off";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(902, 633);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelWeek);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LEO Time";
+            this.TransparencyKey = System.Drawing.SystemColors.ControlLight;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelWeek;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ BigClock/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BigClock
+{
+    public partial class Main : Form
+    {
+        private bool _oddRound = true;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.timerMain.Start();
+            SetTime(true);
+        }
+
+        private void SetTime(bool showComma)
+        {
+            var now = DateTime.Now;
+            this.labelTime.Text = now.ToString(showComma ? "H:mm" : "H:mm"); //"H:mm tt" : "h mm tt");
+            this.labelWeek.Text = now.DayOfWeek.ToString();
+            this.labelDate.Text = now.ToString("yyyy.M.d");
+            //this.labelDate.Text = "2028.22.22";//now.ToString("yyyy.M.d");
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+
+            SetTime(_oddRound);
+            _oddRound = !_oddRound;
+
+            System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString("h:m:s.fff"));
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.timerMain = null;
+            this.Close();
+        }
+
+        //a border-less form movable
+        //ref https://stackoverflow.com/questions/1592876/make-a-borderless-form-movable
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void labelTime_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
